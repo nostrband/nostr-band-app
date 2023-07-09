15db8fa4b75ab662dd9beae61f0dd883bd2e14a4
msultanovdev <mukhammadamin.sultanov@gmail.com>
2023-07-09 19:22:51 +0500

    feat: add show more btn

--- src/components/MarkdownComponent/MarkdownComponent.js
+++ src/components/MarkdownComponent/MarkdownComponent.js
@@ -2,8 +2,16 @@ import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 import { Link } from "react-router-dom";
 import cl from "./MarkdownComponent.module.css";
+import { useState } from "react";
 
 const MarkdownComponent = ({ content }) => {
+  const [isFullContent, setIsFullContent] = useState(false);
+  const MAX_CONTENT_LENGTH = 300;
+
+  const fullContent = isFullContent
+    ? content
+    : content.slice(0, MAX_CONTENT_LENGTH);
+
   const renderLink = ({ children, href }) => {
     return (
       <Link to={href} target="_blanc">
@@ -15,13 +23,23 @@ const MarkdownComponent = ({ content }) => {
   };
 
   return (
-    <ReactMarkdown
-      className={cl.postAbout}
-      remarkPlugins={[remarkGfm]}
-      components={{ a: renderLink }}
-    >
-      {content}
-    </ReactMarkdown>
+    <>
+      <ReactMarkdown
+        className={cl.postAbout}
+        remarkPlugins={[remarkGfm]}
+        components={{ a: renderLink }}
+      >
+        {fullContent}
+      </ReactMarkdown>
+      {content.length > MAX_CONTENT_LENGTH && (
+        <button
+          className={cl.postMoreBtn}
+          onClick={() => setIsFullContent(!isFullContent)}
+        >
+          {isFullContent ? "Hide" : "Show more"}
+        </button>
+      )}
+    </>
   );
 };
 
