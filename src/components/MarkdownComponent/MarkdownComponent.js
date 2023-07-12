e8baf2bfe349a57c6fb4289e8879cd1c868e85a4
msultanovdev <mukhammadamin.sultanov@gmail.com>
2023-07-12 19:47:04 +0500

    fix: fix bugs

--- src/components/MarkdownComponent/MarkdownComponent.js
+++ src/components/MarkdownComponent/MarkdownComponent.js
@@ -6,41 +6,46 @@ import { useState } from "react";
 
 const MarkdownComponent = ({ content }) => {
   const [isFullContent, setIsFullContent] = useState(false);
-  const MAX_CONTENT_LENGTH = 300;
 
-  const fullContent = isFullContent
-    ? content
-    : content.slice(0, MAX_CONTENT_LENGTH);
+  if (content) {
+    const MAX_CONTENT_LENGTH = 300;
 
-  const renderLink = ({ children, href }) => {
+    const fullContent = isFullContent
+      ? content
+      : content.slice(0, MAX_CONTENT_LENGTH);
+
+    const renderLink = ({ children, href }) => {
+      return (
+        <Link to={href} target="_blanc">
+          {children[0]
+            .replace(/^(https|http)?:\/\//, "")
+            .replace(/(.{16}).*(.{10})$/, "$1...$2")}
+        </Link>
+      );
+    };
+
     return (
-      <Link to={href} target="_blanc">
-        {children[0]
-          .replace(/^(https|http)?:\/\//, "")
-          .replace(/(.{16}).*(.{10})$/, "$1...$2")}
-      </Link>
-    );
-  };
-
-  return (
-    <>
-      <ReactMarkdown
-        className={cl.postAbout}
-        remarkPlugins={[remarkGfm]}
-        components={{ a: renderLink }}
-      >
-        {fullContent}
-      </ReactMarkdown>
-      {content.length > MAX_CONTENT_LENGTH && (
-        <button
-          className={cl.postMoreBtn}
-          onClick={() => setIsFullContent(!isFullContent)}
+      <>
+        <ReactMarkdown
+          className={cl.postAbout}
+          remarkPlugins={[remarkGfm]}
+          components={{ a: renderLink }}
         >
-          {isFullContent ? "Hide" : "Show more"}
-        </button>
-      )}
-    </>
-  );
+          {fullContent}
+        </ReactMarkdown>
+        {content.length > MAX_CONTENT_LENGTH && (
+          <button
+            className={cl.postMoreBtn}
+            onClick={() => setIsFullContent(!isFullContent)}
+          >
+            {isFullContent ? "Hide" : "Show more"}
+          </button>
+        )}
+      </>
+    );
+  } else {
+    return "";
+  }
 };
 
 export default MarkdownComponent;
