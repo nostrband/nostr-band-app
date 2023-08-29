d0ce28782fe1b454653487b36294474a7cf161be
msultanovdev <mukhammadamin.sultanov@gmail.com>
2023-08-29 18:25:51 +0500

    finish: finish migration

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+//@ts-ignore
 import App from "./App.tsx";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter } from "react-router-dom";
 import "react-loading-skeleton/dist/skeleton.css";
 import { Provider } from "react-redux";
+//@ts-ignore
 import { setupStore } from "./store/store.ts";
 
 const store = setupStore();
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root")!);
 root.render(
   <>
     <BrowserRouter>
