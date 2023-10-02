84e46605466cb05ff75fb8f2548ff9155dba759b
msultanovdev <mukhammadamin.sultanov@gmail.com>
2023-10-02 17:22:37 +0500

    fix: fix sorts

--- src/utils/sortFunctions.ts
+++ src/utils/sortFunctions.ts
@@ -1,9 +1,12 @@
 import { NDKEvent } from "@nostrband/ndk";
-import { getAllTags } from "./getTags";
 
 export function compareByTagName(a: NDKEvent, b: NDKEvent) {
-  const tagA = a.tags.find((tag) => tag[0] === "d")![1].toLowerCase();
-  const tagB = b.tags.find((tag) => tag[0] === "d")![1].toLowerCase();
+  const tagA = a.tags.find((tag) => tag[0] === "d")
+    ? a.tags.find((tag) => tag[0] === "d")![1].toLowerCase()
+    : a.tags.find((tag) => tag[0] === "l")![1].toLowerCase();
+  const tagB = b.tags.find((tag) => tag[0] === "d")
+    ? b.tags.find((tag) => tag[0] === "d")![1].toLowerCase()
+    : b.tags.find((tag) => tag[0] === "l")![1].toLowerCase();
 
   if (tagA < tagB) {
     return -1;
