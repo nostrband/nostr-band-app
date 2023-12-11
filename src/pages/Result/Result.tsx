713b0b95884203dd2d136e6c9dc977cab96dbb44
msultanovdev <mukhammadamin.sultanov@gmail.com>
2023-12-11 18:05:51 +0500

    feat: add first active

--- src/pages/Result/Result.tsx
+++ src/pages/Result/Result.tsx
@@ -1,19 +1,23 @@
 import { useSearchParams } from "react-router-dom";
 import Profiles from "./Profiles/Profiles";
-import AllResults from "./AllResults/AllResults";
+import AllResults from "./TopResults/TopResults";
 import Zaps from "./Zaps/Zaps";
 import Posts from "./Posts/Posts";
 
 const Result = () => {
   const [searchParams] = useSearchParams();
+  const kindsList = searchParams
+    .get("q")
+    ?.match(/kind:\d+/g)
+    ?.map((kind) => kind.split(":")[1]);
 
-  if (!searchParams.get("type")) {
+  if (!kindsList) {
     return <AllResults />;
-  } else if (searchParams.get("type") === "profiles") {
+  } else if (kindsList.length === 1 && kindsList.includes("0")) {
     return <Profiles />;
-  } else if (searchParams.get("type") === "zaps") {
+  } else if (kindsList.length === 1 && kindsList.includes("9735")) {
     return <Zaps />;
-  } else if (searchParams.get("type") === "posts") {
+  } else if (kindsList.length === 1 && kindsList.includes("1")) {
     return <Posts />;
   }
   return <AllResults />;
